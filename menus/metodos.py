c301b7d01c769b6c0758ccc213c43076bc81b7bd
Áquilas Rodrigues Jucá <aquilasjuca2016@gmail.com>
2024-05-15 17:51:21 -0300

    Adicionando o temporizador.

--- menus/metodos.py
+++ menus/metodos.py
@@ -2,6 +2,7 @@ from assets.node import node
 from assets.tab_move import tab_move
 from assets.utility import utility
 from algorithms.busca_em_largura import busca_em_largura as BFS
+import time
 
 class metodos:
     def __init__(self):
@@ -10,13 +11,20 @@ class metodos:
     def busca_em_largura(self, estado_inicial, estado_final):
         caminho = []
         solver = BFS()
+
+        start_time = time.time()
         caminho = solver.solve(estado_inicial, estado_final)
+        end_time = time.time()
+
+        tempo_gasto = end_time - start_time
         
         if caminho is not None:
             caminho_str = " -> ".join([acao for acao, estado in caminho])
             print("Caminho da resolução: " + caminho_str)
         else:
             print("Não foi encontrada solução")
+
+        print(f"Tempo gasto: {tempo_gasto:.4f} segundos")
     
     def BPI(self, puzzle):
         return
